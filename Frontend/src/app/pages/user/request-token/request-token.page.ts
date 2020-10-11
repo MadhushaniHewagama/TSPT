c733836b42639fdccb510316017c3cfe75303e99
MadhushaniHewagama <hewagamamadhushani@gmail.com>
2020-10-12 00:22:47 +0530

    qr code

--- Frontend/src/app/pages/user/request-token/request-token.page.ts
+++ Frontend/src/app/pages/user/request-token/request-token.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{BarcodeScanner} from '@ionic-native'
 @Component({
   selector: 'app-request-token',
   templateUrl: './request-token.page.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestTokenPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private barcodeScanner: BarcodeScanner) { }
+  start_loc: any=null;
+  end_loc:any=null;
+  cost:any=null;
+  createdCode=null;
+  scannedCode=null;
   ngOnInit() {
+  }
+  
+  createCode(){
+    this.createdCode=this.start_loc;
+  }
+
+  scanCode(){
+    this.barcodeScanner.sca().then(barcodeData =>{
+      this.scannedCode=barcodeData.text;
+    })
   }
 
 }
